4e0fdf85390b55768368e6fe4075dc160de57253
Yeonu <jeongyeonuo.o@gmail.com>
2025-01-21 10:35:53 +0900

    질문 수정하기

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -52,4 +52,14 @@ public class QuestionService {
 		q.setAuthor(user);
 		this.questionRepository.save(q);
 	}
+	
+	public void modify(Question question, String subject, String content) {
+		question.setSubject(subject);
+		question.setContent(content);
+		question.setModifyDate(LocalDateTime.now());
+		this.questionRepository.save(question);
+		
+	}
+	
+	
 }
